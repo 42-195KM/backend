9c913bc0b9c31b8ee8f0d0d7f49da35258aa5619
김재현 <94097685+iconew123@users.noreply.github.com>
2025-04-06 01:23:11 +0900

    #2 feat: common 모듈에 BaseEntity 생성
    - BaseEntity 생성 및 의존성 추가

---
+++ common/src/main/java/com/_42195km/msa/common/BaseEntity.java
@@ -0,0 +1,60 @@
+package com._42195km.msa.common;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@Column(nullable = false)
+	private boolean isDeleted = false;
+
+	private LocalDateTime deletedAt;
+
+	@CreatedBy
+	@Column(updatable = false)
+	private UUID createdBy;
+
+	@LastModifiedBy
+	private UUID updatedBy;
+
+	private UUID deletedBy;
+
+	// @PreUpdate
+	// public void setDeleted() {
+	// 	if (isDeleted) {
+	// 		deletedAt = LocalDateTime.now();
+	// 		deletedBy = getAuthenticatedUsername();
+	// 	}
+	// }
+	//
+	// private String getAuthenticatedUsername() {
+	// 	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	// 	if (authentication == null || !authentication.isAuthenticated()) {
+	// 		return "SYSTEM";
+	// 	}
+	// 	Object principal = authentication.getPrincipal();
+	// 	return (principal instanceof UserDetails) ? ((UserDetails) principal).getUsername() : "UNKNOWN";
+	// }
+
+}
