600003c474a224c256c970fefbe02f02b69253a7
sodychoe <sodychoe@icloud.com>
2025-04-07 14:49:23 +0900

    feat: recap 서비스 초기설정 구축 #11

---
+++ userrecap-service/src/main/java/com/_42195km/msa/userrecapservice/UserrecapServiceApplication.java
@@ -0,0 +1,13 @@
+package com._42195km.msa.userrecapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrecapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserrecapServiceApplication.class, args);
+	}
+
+}
