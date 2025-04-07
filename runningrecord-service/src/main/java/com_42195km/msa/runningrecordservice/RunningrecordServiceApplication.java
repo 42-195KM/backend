596298d19c3f89c035a05fe3db8e95a3b36c9d3f
zzangkkmin <zzangkkmin@naver.com>
2025-04-07 17:27:05 +0900

    feat: 러닝 기록 도메인 추가 #16
    - spring boot를 3.4.4로 변경

---
+++ runningrecord-service/src/main/java/com_42195km/msa/runningrecordservice/RunningrecordServiceApplication.java
@@ -0,0 +1,13 @@
+package com_42195km.msa.runningrecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningrecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningrecordServiceApplication.class, args);
+    }
+
+}
