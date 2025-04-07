814add21fc694665c4fda1c00ef72de89e5e43a0
zzangkkmin <zzangkkmin@naver.com>
2025-04-07 21:03:33 +0900

    feat: 업적 도메인 추가 #13

---
+++ achivement-service/src/main/java/com_42195km/msa/achivementservice/AchivementServiceApplication.java
@@ -0,0 +1,13 @@
+package com_42195km.msa.achivementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchivementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchivementServiceApplication.class, args);
+	}
+
+}
---
+++ achivement-service/src/test/java/com_42195km/msa/achivementservice/AchivementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com_42195km.msa.achivementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchivementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
