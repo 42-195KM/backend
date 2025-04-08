d3bfa5a981c15a3b33712bd0dd5d0e33eb330c4f
김재현 <94097685+iconew123@users.noreply.github.com>
2025-04-09 04:02:50 +0900

    feat: User-Service DDD 구조 유저 INFO 수정 API 구현 #15

--- user-service/src/main/java/com/_42195km/msa/user/application/service/UserServiceImpl.java
+++ user-service/src/main/java/com/_42195km/msa/user/application/service/UserServiceImpl.java
@@ -10,14 +10,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com._42195km.msa.common.exception.CustomBusinessException;
 import com._42195km.msa.user.application.dto.request.CreateUserRequestDto;
+import com._42195km.msa.user.application.dto.request.UpdateUserRequestDto;
 import com._42195km.msa.user.application.dto.response.CreateUserResponseDto;
 import com._42195km.msa.user.application.dto.response.GetAllUserResponseDto;
 import com._42195km.msa.user.application.dto.response.GetUserResponseDto;
 import com._42195km.msa.user.application.dto.response.SearchUserResponseDto;
+import com._42195km.msa.user.application.dto.response.UpdateUserResponseDto;
 import com._42195km.msa.user.domain.exception.UserException;
 import com._42195km.msa.user.domain.model.User;
 import com._42195km.msa.user.infrastructure.persistence.UserRepositoryImpl;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,7 +35,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	@Transactional
-	public CreateUserResponseDto createUser(CreateUserRequestDto createUserRequestDto) {
+	public CreateUserResponseDto createUser(@Valid CreateUserRequestDto createUserRequestDto) {
 
 		// TODO : 닉네임 중복 확인
 
@@ -84,6 +87,17 @@ public class UserServiceImpl implements UserService {
 		);
 
 		return searchUserResponseDtos;
+	}
+
+	@Transactional
+	@Override
+	public UpdateUserResponseDto updateUser(UUID userId, UpdateUserRequestDto updateUserRequestDto) {
+
+		User targetUser = findUserById(userId);
+
+		targetUser.update(updateUserRequestDto);
+
+		return UpdateUserResponseDto.fromUser(targetUser);
 	}
 
 	private User findUserById(UUID userId) {
--- user-service/src/main/java/com/_42195km/msa/user/presentation/controller/UserController.java
+++ user-service/src/main/java/com/_42195km/msa/user/presentation/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +20,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com._42195km.msa.common.api.ApiResponse;
 import com._42195km.msa.user.application.dto.request.CreateUserRequestDto;
+import com._42195km.msa.user.application.dto.request.UpdateUserRequestDto;
 import com._42195km.msa.user.application.dto.response.CreateUserResponseDto;
 import com._42195km.msa.user.application.dto.response.GetAllUserResponseDto;
 import com._42195km.msa.user.application.dto.response.GetUserResponseDto;
 import com._42195km.msa.user.application.dto.response.SearchUserResponseDto;
+import com._42195km.msa.user.application.dto.response.UpdateUserResponseDto;
 import com._42195km.msa.user.application.service.UserService;
 import com._42195km.msa.user.domain.success.UserSuccessCode;
 
@@ -116,6 +119,26 @@ public class UserController {
 					.code(UserSuccessCode.FIND_KEYWORD_USER_LIST_SUCCESS.getCode())
 					.message(UserSuccessCode.FIND_KEYWORD_USER_LIST_SUCCESS.getMessage())
 					.data(searchList)
+					.build()
+			);
+	}
+
+	@PatchMapping("/v1/users/{userId}")
+	public ResponseEntity<ApiResponse<UpdateUserResponseDto>> updateUser(
+		@PathVariable UUID userId,
+		@RequestBody @Valid UpdateUserRequestDto updateUserRequestDto
+	) {
+
+		UpdateUserResponseDto updateUserResponseDto = userService.updateUser(userId, updateUserRequestDto);
+
+		return ResponseEntity
+			.ok(
+				ApiResponse
+					.<UpdateUserResponseDto>builder()
+					.status(UserSuccessCode.UPDATE_USER_SUCCESS.getStatusCode().value())
+					.code(UserSuccessCode.UPDATE_USER_SUCCESS.getCode())
+					.message(UserSuccessCode.UPDATE_USER_SUCCESS.getMessage())
+					.data(updateUserResponseDto)
 					.build()
 			);
 	}
