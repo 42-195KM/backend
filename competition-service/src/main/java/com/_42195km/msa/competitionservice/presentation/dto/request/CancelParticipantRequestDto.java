f5983d66f18560389df07bec39343b9e35d9d10b
Kim jisu <jisu0259@naver.com>
2025-04-11 16:13:15 +0900

    test: participant service test #30

--- competition-service/src/main/java/com/_42195km/msa/competitionservice/presentation/dto/request/CancelParticipantRequestDto.java
+++ competition-service/src/main/java/com/_42195km/msa/competitionservice/presentation/dto/request/CancelParticipantRequestDto.java
@@ -4,12 +4,14 @@ import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CancelParticipantRequestDto {
