d3c0d50b3ca9081f5609c10feb0690137606f5ee
kdh610 <k_dh610@naver.com>
2025-04-14 20:50:13 +0900

    refactor: dto mapper #43

--- chatbot-service/src/main/java/com/_42195km/msa/chatbotservice/presentation/ChatbotController.java
+++ chatbot-service/src/main/java/com/_42195km/msa/chatbotservice/presentation/ChatbotController.java
@@ -2,16 +2,20 @@ package com._42195km.msa.chatbotservice.presentation;
 
 
 
+import com._42195km.msa.chatbotservice.application.dto.response.SearchConversationResponseAppDto;
 import com._42195km.msa.chatbotservice.application.service.AiService;
 import com._42195km.msa.chatbotservice.application.service.ConversationService;
 import com._42195km.msa.chatbotservice.application.service.EmbeddingService;
 import com._42195km.msa.chatbotservice.application.service.SseService;
 import com._42195km.msa.chatbotservice.domain.entity.Conversation;
 import com._42195km.msa.chatbotservice.presentation.dto.request.QuestionRequestDto;
-import com._42195km.msa.chatbotservice.presentation.dto.request.SearchConversationDto;
+import com._42195km.msa.chatbotservice.presentation.dto.request.SearchConversationRequestDto;
+import com._42195km.msa.chatbotservice.presentation.dto.response.SearchConversationResponseDto;
 import com._42195km.msa.chatbotservice.presentation.mapper.ChatbotMapper;
+import com._42195km.msa.chatbotservice.presentation.mapper.ConversationMapper;
+import com._42195km.msa.common.api.ApiResponse;
+import com._42195km.msa.common.code.CommonServiceCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,20 +45,22 @@ public class ChatbotController {
     }
 
     @GetMapping("/conversations/search")
-    public ResponseEntity<Page<Conversation>> search(@RequestBody SearchConversationDto searchConversationDto,
+    public ResponseEntity<ApiResponse<Page<SearchConversationResponseDto>>> search(@RequestBody SearchConversationRequestDto searchConversationDto,
                                                      @RequestParam(value = "page", defaultValue = "1") int page,
                                                      @RequestParam(value = "size", defaultValue = "10") int size,
                                                      @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
                                                      @RequestParam(value = "isAsc", defaultValue = "false") boolean isAsc){
-        Page<Conversation> search = conversationService.search(ChatbotMapper.toSearchConversationAppDto(searchConversationDto), page-1, size, sortBy, isAsc);
-        return ResponseEntity.ok(search);
+        Page<SearchConversationResponseAppDto> search = conversationService.search(ConversationMapper.toAppDto(searchConversationDto), page-1, size, sortBy, isAsc);
+        return ResponseEntity.ok(ApiResponse.success(search.map(ConversationMapper::toDto)));
     }
 
 
     @PostMapping("/embedding")
     public ResponseEntity<?> saveEmbeddingInfo(@RequestBody String embeddingRequest){
         embeddingService.saveEmbeddingInfo(embeddingRequest);
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(ApiResponse.builder()
+                                        .code(CommonServiceCode.SUCCESS.getCode())
+                                        .build());
 
     }
 
--- chatbot-service/src/main/java/com/_42195km/msa/chatbotservice/presentation/dto/request/SearchConversationRequestDto.java
+++ chatbot-service/src/main/java/com/_42195km/msa/chatbotservice/presentation/dto/request/SearchConversationRequestDto.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 @Builder
 @Getter
-public class SearchConversationDto {
+public class SearchConversationRequestDto {
 
     private UUID userId;
     private LocalDateTime createdAt;
