489494587dd155a6dec167445000b5d04e95a712
sodychoe <sodychoe@icloud.com>
2025-04-15 13:28:52 +0900

    feat: feign-client 연결 설정 추가

--- userrecap-service/src/main/java/com/_42195km/msa/userrecapservice/UserrecapServiceApplication.java
+++ userrecap-service/src/main/java/com/_42195km/msa/userrecapservice/UserrecapServiceApplication.java
@@ -2,8 +2,10 @@ package com._42195km.msa.userrecapservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com._42195km.msa.userrecapservice", "com._42195km.msa.common"})
+@EnableFeignClients
 public class UserrecapServiceApplication {
 
 	public static void main(String[] args) {
