db2351aeec4e4def887a8655800703dfe658f06c
zzangkkmin <zzangkkmin@naver.com>
2025-04-16 11:56:15 +0900

    chore: running-record, acheivement 에도 추가

--- runningrecord-service/src/main/java/com/_42195km/msa/runningrecordservice/RunningrecordServiceApplication.java
+++ runningrecord-service/src/main/java/com/_42195km/msa/runningrecordservice/RunningrecordServiceApplication.java
@@ -2,8 +2,12 @@ package com._42195km.msa.runningrecordservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com._42195km.msa.runningrecordservice", "com._42195km.msa.common"})
+@EnableFeignClients
+@EnableDiscoveryClient
 public class RunningrecordServiceApplication {
 
     public static void main(String[] args) {
