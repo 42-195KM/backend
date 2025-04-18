e1f659b86ce6c5f11a61f11db72a7bdd701b3251
김재현 <94097685+iconew123@users.noreply.github.com>
2025-04-18 14:45:38 +0900

    fix: 카프카 JsonSerializer import 변경 #69

--- user-service/src/main/java/com/_42195km/msa/user/infrastructure/config/KafkaConfig.java
+++ user-service/src/main/java/com/_42195km/msa/user/infrastructure/config/KafkaConfig.java
@@ -12,8 +12,9 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+// import com.fasterxml.jackson.databind.JsonSerializer;
 
 @Configuration
 @EnableKafka
