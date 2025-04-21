3bc8daba0e6e9ecb1646eaadee17fbc1299a90e2
zzangkkmin <zzangkkmin@naver.com>
2025-04-21 11:48:25 +0900

    fix: 서비스 실행자를 공통 모듈로 #65
    - 업적 서비스 리팩토링과 같이 작업

---
+++ common/src/main/java/com/_42195km/msa/common/service/ServiceExecutor.java
@@ -0,0 +1,16 @@
+package com._42195km.msa.common.service;
+
+import java.util.function.Supplier;
+
+import com._42195km.msa.common.code.ServiceCode;
+import com._42195km.msa.common.exception.CustomBusinessException;
+
+public abstract class ServiceExecutor {
+	protected <T> T execute(Supplier<T> action, ServiceCode code) {
+		try {
+			return action.get();
+		} catch (Exception e){
+			throw CustomBusinessException.from(code);
+		}
+	}
+}
