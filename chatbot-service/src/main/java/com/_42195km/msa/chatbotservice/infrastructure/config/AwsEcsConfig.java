0a2df398a171fe5142a65e8d94df4436038af487
sodychoe <sodychoe@icloud.com>
2025-04-29 12:45:08 +0900

    feat : ECS 서비스가 아이피를 알 수 있도록 설정

---
+++ chatbot-service/src/main/java/com/_42195km/msa/chatbotservice/infrastructure/config/AwsEcsConfig.java
@@ -0,0 +1,35 @@
+package com._42195km.msa.chatbotservice.infrastructure.config;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Profile("aws")
+@Slf4j
+public class AwsEcsConfig {
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = new EurekaInstanceConfigBean(inetUtils);
+		String ip = null;
+
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+			log.info("ECS Task Container Private Ip Address = {}", ip);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		eurekaInstanceConfigBean.setIpAddress(ip);
+		eurekaInstanceConfigBean.setPreferIpAddress(true);
+
+		return eurekaInstanceConfigBean;
+	}
+}
