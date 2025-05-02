43640b757839a32d3d6c1ada826ca6e978f39e2e
sodychoe <sodychoe@icloud.com>
2025-05-02 11:48:46 +0900

    feat : eureka discovery 설정

--- userrecap-service/src/main/java/com/_42195km/msa/userrecapservice/UserrecapServiceApplication.java
+++ userrecap-service/src/main/java/com/_42195km/msa/userrecapservice/UserrecapServiceApplication.java
@@ -3,9 +3,11 @@ package com._42195km.msa.userrecapservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com._42195km.msa.userrecapservice", "com._42195km.msa.common"})
 @EnableFeignClients
+@EnableScheduling
 public class UserrecapServiceApplication {
 
 	public static void main(String[] args) {
